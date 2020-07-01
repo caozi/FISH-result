e5f4489cb16d0fb1f449eec590da18e3fde3a15f
caozi <imcaozi@outlook.com>
2020-07-01 08:25:28 +0800

    modify models

--- weixin_test/models.py
+++ weixin_test/models.py
@@ -8,24 +8,15 @@ class Result(models.Model):
         return self.result
 
 
-class Test1(models.Model):
-    test_name = models.CharField(max_length=20)
-    test_result = models.ForeignKey(Result, on_delete=models.CASCADE)
+class TestItem(models.Model):
+    test_item = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.test_name
+        return self.test_item
 
 
-class Test2(models.Model):
-    test_name = models.CharField(max_length=20)
-    test_result = models.ForeignKey(Result, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.test_name
-
-
-class Test3(models.Model):
-    test_name = models.CharField(max_length=20)
+class Test(models.Model):
+    test_name = models.ForeignKey(TestItem, unique=True)
     test_result = models.ForeignKey(Result, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -37,9 +28,9 @@ class Patient(models.Model):
     patient_name = models.CharField(max_length=20)
     patient_pathology_number = models.CharField(max_length=10)
     patient_hospital_number = models.CharField(max_length=10)
-    patient_test_1 = models.ForeignKey(Test1, on_delete=models.CASCADE)
-    patient_test_2 = models.ForeignKey(Test2, on_delete=models.CASCADE)
-    patient_test_3 = models.ForeignKey(Test3, on_delete=models.CASCADE)
+    patient_test_1 = models.ForeignKey(Test, on_delete=models.CASCADE)
+    patient_test_2 = models.ForeignKey(Test, on_delete=models.CASCADE)
+    patient_test_3 = models.ForeignKey(Test, on_delete=models.CASCADE)
     patient_note = models.CharField(max_length=200)
 
     def __str__(self):
